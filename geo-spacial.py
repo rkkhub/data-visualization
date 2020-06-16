bcca45cadea3cf777860d82468838a6f14a8c865
unknown <rkkmailbox@gmail.com>
2020-06-17 03:50:08 +0530

    final commit

--- geo-spacial.py
+++ geo-spacial.py
@@ -3,25 +3,14 @@ from bokeh.io import show
 from bokeh.models import ColumnDataSource, HoverTool, LogColorMapper
 from pathlib import Path
 import geopandas as gpd
-from shapely.geometry import Polygon
 import pandas as pd
 import json
 
-file_shapefile = Path("dependency/usa-states-2018/usa-states-2018.shp")
-file_save = Path("dependency/usa-states-2018/data.json")
 
-df_states = gpd.read_file(file_shapefile)
-names = [state for state in df_states.NAME]
-states, polygons = [], []
-[states.append(state) if type(item) == Polygon else [states.append(state) for i in list(item)] for item, state in zip(df_states.geometry, names)]
-[polygons.append(item) if type(item) == Polygon else [polygons.append(i) for i in list(item)] for item in df_states.geometry]
-xs, ys = [], []
-xs = [list(polygon.boundary.coords.xy[0]) for polygon in polygons]
-ys = [list(polygon.boundary.coords.xy[1]) for polygon in polygons]
+file_data = Path("dependency/usa-states-2018.json")
 
-data = dict(xs = xs, ys = ys, states = states)
-# with open(file_save, 'w') as f:
-#     json.dump(data, f)
+with open(file_data,"r") as f:
+  data = json.load(f)
 
 source = ColumnDataSource(data)
 p = figure(title="geo-spacial visualization",
@@ -35,6 +24,7 @@ p.patches(source=source,
           fill_alpha=0.9,
           line_color="white",
           line_width=0.5)
+
 p.axis.axis_label = None
 p.sizing_mode = "scale_both"
 p.axis.visible = False
--- preprocess-shapefile.py
+++ preprocess-shapefile.py
@@ -1,12 +1,44 @@
 from pathlib import Path
+from shapely.geometry import Polygon
 import geopandas as gpd
+import json
 
-file_shapefile = Path("source_file.shp")
-df_states = gpd.read_file(file_shapefile)
-df_states = df_states.cx[-125:-69,25.5:49.5]                                    # selecting only required shapes/locations
-df_states.drop(["column1","column2","column3"], axis=1, inplace = True)         # drop columns that are not required
-df_states.to_crs(epsg=2163, inplace=True)                                       # convert projection to a desired espg
-df_states.to_file(driver = 'ESRI Shapefile', filename= Path("targed_file.shp")) # save the processed shapefie
+file_source = Path("dependency/cb_2018_us_state_500k/cb_2018_us_state_500k.shp")
+file_formated = Path("dependency/usa-states-2018/usa-states-2018.shp")
+file_json = Path("dependency/usa-states-2018.json")
+
+"""
+Source shapefile contain excess data, and requires projection conversion
+"""
+
+df_states = gpd.read_file(file_source)
+df_states = df_states.cx[-125:-69, 25.5:49.5]                                       # selecting only required shape/location coordinates
+df_states.drop(["STATEFP", "STATENS", "AFFGEOID","GEOID",
+                "STUSPS", "LSAD", "ALAND", "AWATER"],
+                axis=1, inplace = True)
+df_states.to_crs(epsg=2163, inplace=True)                                           # convert projection to a desired espg
+
+df_states.to_file(driver = 'ESRI Shapefile',                                        # save processed shapefie
+                  filenam e= file_formated)
 
 
+"""
+Polygons and multipolygons need to be converted to x and y coordinates for plotting. 
+"""
+
+names = [state for state in df_states.NAME]
+polygons, states = [], []
+[polygons.append(item) if type(item) == Polygon 
+else [polygons.append(i) for i in list(item)]
+for item in df_states.geometry]                                                     # convert multipolygons to polygons
+[states.append(state) if type(item) == Polygon
+else [states.append(state) for i in list(item)]
+for item, state in zip(df_states.geometry, names)]                                  # assign state names for polygons extracted from multipolygons
+xs, ys = [], []
+xs = [list(polygon.boundary.coords.xy[0]) for polygon in polygons]
+ys = [list(polygon.boundary.coords.xy[1]) for polygon in polygons]
+data = dict(xs = xs, ys = ys, states = states)
+
+with open(file_json, 'w') as f:                                                     # save extracted x and y coordinates 
+    json.dump(data, f)
 
